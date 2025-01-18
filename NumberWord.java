64ba6202d48e1f765dc7d1edfb804d228e9dedb4
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-01-18 15:07:35 +0530

    second commit

---
+++ NumberWord.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class NumberWord{
+public static void main(String arg[]){
+	Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
+	switch(num){
+		case  1:
+		System.out.print("One");
+		break;
+     		case  2:
+                System.out.print("Two");
+                break;
+		case  3:
+                System.out.print("Three");
+                break;
+		case  4:
+                System.out.print("four");
+                break;
+		case  5:
+                System.out.print("five");
+                break;
+		 case  6:
+                System.out.print("six");
+                break;
+                case  7:
+                System.out.print("seven");
+                break;
+                case  8:
+                System.out.print("Eight");
+                break;
+                case  9:
+                System.out.print("Nine");
+                break;
+                default:
+                 System.out.print("Zero");
+               
+
+
+	}
+} 
+}
+
+
