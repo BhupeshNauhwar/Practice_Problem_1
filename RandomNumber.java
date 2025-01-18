4077fececff33252c4be55fafaf6eb13bc7e5577
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-01-18 15:24:59 +0530

    RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -5,11 +5,15 @@ public class  RandomNumber{
 		Random rm=new Random();
 		int  min=Integer.MAX_VALUE;
 		int max=Integer.MIN_VALUE;
+		System.out.print("Random numbers are ");
 		for(int i=1;i<=5;i++){
 			int n=rm.nextInt(900)+100;
+			System.out.print(n+" ");
 			min=Math.min(min,n);
 			max=Math.max(max,n);
+
 		}
+		System.out.println();
 		System.out.println("Minimum number is "+min);
 		System.out.println("Maximum number is "+max);
 		
