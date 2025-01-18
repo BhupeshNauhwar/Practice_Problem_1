9eb6841e69902da238319f13d14f08496a65c39f
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-01-18 14:57:38 +0530

    commit

---
+++ PracticeProblem.java
@@ -0,0 +1,17 @@
+public class PracticeProblem{
+	public static void main(String arg[]){
+		for(int i=1;i<=10;i++){
+			if(i%2==0){
+				System.out.println("Even "+i);
+			}
+			else{
+				System.out.println("Odd " +i);
+			}
+		}
+
+	}
+
+
+}
+
+
